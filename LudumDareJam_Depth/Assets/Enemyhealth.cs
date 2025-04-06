d5321473d96cd44134b4cebd99a14756b04d5bb4
Jet Kingston <jettaikingston10@gmail.com>
2025-04-06 22:16:58 +0200

    Enemies can now die to 2 hits of player attack
    Players can now attack enemies and enemies die. Bit buggy since enemies dont stop moving and can still kill player but will fix that later.

--- LudumDareJam_Depth/Assets/Enemyhealth.cs
+++ LudumDareJam_Depth/Assets/Enemyhealth.cs
@@ -1,21 +1,27 @@
 using UnityEngine;
-
 public class Enemyhealth : MonoBehaviour
 {
-
     public float health;
+    private Animator anim;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Initialize the animator component
+        anim = GetComponent<Animator>();
+        if (anim == null) {
+            Debug.LogWarning("No Animator component found on this enemy!");
+        }
     }
-
+    
     // Update is called once per frame
     void Update()
     {
         if(health <= 0){
             Debug.Log("enemy dead");
+            if (anim != null) {
+                anim.SetBool("isDead", true);
+            }
         }
-        
     }
 }
